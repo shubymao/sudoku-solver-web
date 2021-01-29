14146e56ea1f6846bdec4e5311cf135665fe8bd6
shubymao <le-li.mao@mail.mcgill.ca>
2021-01-28 21:37:00 -0800

    added the row, col, and box set function

--- src/solver.ts
+++
@@ -1,7 +0,0 @@
-
-
-
-export default function solve(grid: Array<Array<Number>>){
-    if (grid.length !== 9 || grid[0].length !== 9) throw new Error('Input Size Not Valid');
-    return grid;
-}
--- tests/solver.test.ts
+++ tests/solver.test.ts
@@ -1,18 +1,25 @@
 import { expect } from 'chai';
-import solve from '../src/solver'
+import solve from '../src/index'
 
 describe('solve sudoku', function () {
     it('accept valid size', function () {
-        let grid = new Array<Array<Number>>(9);
+        let grid = new Array<Array<number>>(9);
         for (let i = 0; i < 9; i++) {
-            grid[i] = new Array<Number>(9);
+            grid[i] = new Array<number>(9);
         }
         expect(function(){solve(grid);}).not.throws();
     });
-    it('reject valid size', function () {
-        let grid = new Array<Array<Number>>(8);
+    it('reject invalid row size', function () {
+        let grid = new Array<Array<number>>(8);
         for (let i = 0; i < 8; i++) {
-            grid[i] = new Array<Number>(9);
+            grid[i] = new Array<number>(9);
+        }
+        expect(function(){solve(grid);}).throws();
+    });
+    it('reject invalid col size', function () {
+        let grid = new Array<Array<number>>(9);
+        for (let i = 0; i < 9; i++) {
+            grid[i] = new Array<number>(8);
         }
         expect(function(){solve(grid);}).throws();
     });
