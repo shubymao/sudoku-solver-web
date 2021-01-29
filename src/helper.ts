e7db60e7e5e5011e523aa68f21abd75748dbd442
shubymao <le-li.mao@mail.mcgill.ca>
2021-01-28 21:53:48 -0800

    added linter and pritter to the package.json

--- src/helper.ts
+++ src/helper.ts
@@ -5,7 +5,7 @@ export const BOX_SIZE = 3;
 export function getRowSet(grid: number[][], row: number) {
     let set = 0;
     for (let i = 0; i < SIZE; i++) {
-        if (grid[row][i] == 0) continue;
+        if (!grid[row][i] || grid[row][i] === 0) continue;
         let mask = 1 << (grid[row][i] - 1);
         if ((set & mask) !== 0) throw new Error(`Invalid grid row ${row} contain multiple ${grid[row][i]}`);
         set = set | mask;
@@ -16,7 +16,7 @@ export function getRowSet(grid: number[][], row: number) {
 export function getColSet(grid: number[][], col: number) {
     let set = 0;
     for (let i = 0; i < SIZE; i++) {
-        if (grid[i][col] == 0) continue;
+        if (!grid[i][col] || grid[i][col] === 0) continue;
         let mask = 1 << (grid[i][col] - 1);
         if ((set & mask) !== 0) throw new Error(`Invalid grid col ${col} contain multiple ${grid[i][col]}`);
         set = set | mask;
@@ -30,7 +30,7 @@ export function getBoxSet(grid: number[][], box: number) {
     let col = box % 3;
     for (let i = 0; i < BOX_SIZE; i++) {
         for (let j = 0; j < BOX_SIZE; j++) {
-            if (grid[row + i][col + j] == 0) continue;
+            if (!grid[row + i][col + j] || grid[row + i][col + j] == 0) continue;
             let mask = 1 << (grid[row + i][col + j] - 1);
             if ((set & mask) !== 0) {
                 let value = grid[row + i][col + j];
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { getBoxSet, getColSet, getRowSet, SIZE } from "./helper";
 
 
-export default function solve(grid: Array<Array<number>>){
+export default function solve(grid: Array<Array<number>>) {
     if (grid.length !== SIZE || grid[0].length !== SIZE) throw new Error('Input Size Not Valid');
     let rowSets = new Array<number>(SIZE);
     let colSets = new Array<number>(SIZE);
     let boxSets = new Array<number>(SIZE);
-    for(let i = 0; i < SIZE ; i++){
-        rowSets[i] = getRowSet(grid,i);
-        colSets[i] = getColSet(grid,i);
-        boxSets[i] = getBoxSet(grid,i);
+    for (let i = 0; i < SIZE; i++) {
+        rowSets[i] = getRowSet(grid, i);
+        colSets[i] = getColSet(grid, i);
+        boxSets[i] = getBoxSet(grid, i);
     }
     return grid;
 }
